0c378ca0cef969e41c80de80544423afa4f35e52
bcher31-hub <bcher31@wgu.edu>
2026-04-13 00:45:48 +0000

    fix: migrate next image config to remotePatterns for Vercel

---
+++ .next/server/chunks/ssr/app_report_page_072sry~.js
@@ -0,0 +1,3 @@
+module.exports=[32636,a=>{"use strict";a.i(87924),a.i(72131);let b=Error("Cannot find module '@/lib/supabaseClient'");throw b.code="MODULE_NOT_FOUND",b}];
+
+//# sourceMappingURL=app_report_page_072sry~.js.map
