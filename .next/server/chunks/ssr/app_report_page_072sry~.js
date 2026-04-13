36c863ff9296961842c7749d00ea14bfba836f7d
bcher31-hub <bcher31@wgu.edu>
2026-04-13 01:53:53 +0000

    fix: enable TypeScript support and clean build artifacts

--- .next/server/chunks/ssr/app_report_page_072sry~.js
+++
@@ -1,3 +0,0 @@
-module.exports=[32636,a=>{"use strict";a.i(87924),a.i(72131);let b=Error("Cannot find module '@/lib/supabaseClient'");throw b.code="MODULE_NOT_FOUND",b}];
-
-//# sourceMappingURL=app_report_page_072sry~.js.map
