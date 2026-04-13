7d32da2bbcf78dfe82f71e0470b215afb822972b
bcher31-hub <bcher31@wgu.edu>
2026-04-13 15:57:15 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,19 +1,36 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Map from "@/components/Map";
 import PetFeed from "@/components/PetFeed";
+import UploadForm from "@/components/UploadForm";
 
 export default function Home() {
   const [pets, setPets] = useState([]);
 
   return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>🐾 PawSignal</h1>
+    <div style={{ maxWidth: "1000px", margin: "0 auto" }}>
+      
+      {/* HEADER */}
+      <h1 style={{ textAlign: "center", marginTop: 20 }}>
+        🐾 PawSignal
+      </h1>
 
-      <Map pets={pets} setPets={setPets} />
+      {/* UPLOAD SECTION */}
+      <section style={{ marginBottom: 20 }}>
+        <UploadForm />
+      </section>
 
-      <PetFeed pets={pets} />
+      {/* MAP SECTION */}
+      <section style={{ marginBottom: 20 }}>
+        <Map pets={pets} setPets={setPets} />
+      </section>
+
+      {/* FEED SECTION */}
+      <section>
+        <PetFeed pets={pets} />
+      </section>
+
     </div>
   );
 }
