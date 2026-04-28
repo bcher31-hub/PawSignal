9565c1a3eb85458fbb011a8d6443a6c41b3a929a
bcher31-hub <bcher31@wgu.edu>
2026-04-28 13:58:04 -0400

    Create TopBar.js

---
+++ components/TopBar.js
@@ -0,0 +1,16 @@
+export default function TopBar() {
+  return (
+    <div style={styles.bar}>
+      PawSignal
+    </div>
+  );
+}
+
+const styles = {
+  bar: {
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: 700,
+    paddingBottom: 4,
+  },
+};
